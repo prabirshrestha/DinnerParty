2fe7c665141273160ef88f7ec39e42842e2ff4a0
prabirshrestha <mail@prabir.me>
2012-07-04 13:18:06 -0400

    updated jquery.ui humanity to 1.8.21 and uses cassette fallback

--- CassetteConfiguration.cs
+++ CassetteConfiguration.cs
@@ -9,11 +9,16 @@ namespace DinnerParty
     /// </summary>
     public class CassetteBundleConfiguration : IConfiguration<BundleCollection>
     {
+        public const string JQueryUiCssPath = "assets/stylesheets/jquery.ui/humanity/jquery-ui-1.8.21.custom.css";
+
         public void Configure(BundleCollection bundles)
         {
             // Please read http://getcassette.net/documentation/configuration
 
             bundles.Add<StylesheetBundle>("assets/stylesheets/style.less");
+            bundles.Add<StylesheetBundle>(JQueryUiCssPath);
+
+            // note: never include modernizr from cdn, coz it is never minified and it isn't meant to be included from cdn
             bundles.Add<ScriptBundle>("assets/javascripts/header", b => b.PageLocation = "header");
         }
     }
