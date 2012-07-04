c64b3cb433f7edb63da8ed9735d848dc59b0c211
prabirshrestha <mail@prabir.me>
2012-07-04 13:55:15 -0400

    added date.format.js, nerddinner.js and timepicketaddon.js in app.js

--- assets/javascripts/app/app.js
+++ assets/javascripts/app/app.js
@@ -1,7 +1,14 @@
-﻿App = {
+﻿// @reference _libs/date.format.js
+// @reference _libs/NerdDinner.js
+// @reference _libs/timepicker_plug/TimePickerAddOn.js
+
+
+App = {
     
     initialize: function(options) {
         options || (options = { });
     }
 
 };
+
+
